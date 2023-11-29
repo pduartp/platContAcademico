f679bdb43b7db32769113682893bdfe0888142df
pduartp <patrick.d.pimenta@gmail.com>
2023-11-29 13:18:12 -0300

    Create Bug CSS

--- Data/Postagem.cs
+++ Data/Postagem.cs
@@ -1,5 +1,12 @@
 public class Postagem{
 
+    public Postagem(){}
+    public Postagem(int id,int idusuario, string descricao){
+        this.Id = id;
+        this.IdUsuario = idusuario;
+        this.Descricao = descricao;
+    }
+
     public int Id {get; set;}
     public int IdUsuario {get; set;}
     public string? Descricao {get; set;}
